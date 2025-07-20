ed7be3ed9341c00c6c14a2bfe6bf511f1493462a
Klimchuk_Sergei <klim.work@mail.ru>
2025-07-20 22:03:56 +0300

    Написан функционал обработки post и get

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
-# Create your views here.
+# catalog/views.py
+
+from django.shortcuts import render
+
+
+def home(request):
+
+    return render(request, 'home.html')
+
+
+def contacts(request):
+
+    if request.method == 'POST':
+
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+
+        print(f"Новое сообщение от пользователя {name} (email: {email}): {message}")
+
+        context = {
+            'success': True
+        }
+
+        return render(request, 'contacts.html', context)
+
+    return render(request, 'contacts.html')
