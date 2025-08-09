a6c576516e99e2ac63c281b6829dd76f8eb35c25
Klimchuk_Sergei <klim.work@mail.ru>
2025-08-09 21:01:29 +0300

    views + urls

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
+from .models import Blog
 
-# Create your views here.
+class BlogCreateView(CreateView):
+    model = Blog
+    fields = ('title', 'content', 'preview', 'is_published')
+    success_url = reverse_lazy('blog:list')
+
+class BlogUpdateView(UpdateView):
+    model = Blog
+    fields = ('title', 'content', 'preview', 'is_published')
+
+class BlogListView(ListView):
+    model = Blog
+
+class BlogDetailView(DetailView):
+    model = Blog
+
+class BlogDeleteView(DeleteView):
+    model = Blog
+    success_url = reverse_lazy('blog:list')
