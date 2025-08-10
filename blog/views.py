b4a970dc1620f3128563e002624f71fe0247308c
Klimchuk_Sergei <klim.work@mail.ru>
2025-08-10 22:03:53 +0300

    финал. проверен на работу по критериям

--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,43 @@
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
 from .models import Blog
+
 
 class BlogCreateView(CreateView):
     model = Blog
     fields = ('title', 'content', 'preview', 'is_published')
     success_url = reverse_lazy('blog:list')
 
+
 class BlogUpdateView(UpdateView):
     model = Blog
     fields = ('title', 'content', 'preview', 'is_published')
 
+    # Перенаправление после редактирования
+    def get_success_url(self):
+        return reverse('blog:detail', args=[self.kwargs.get('pk')])
+
+
 class BlogListView(ListView):
     model = Blog
 
+    # Фильтрация опубликованных статей
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset(*args, **kwargs)
+        queryset = queryset.filter(is_published=True)
+        return queryset
+
+
 class BlogDetailView(DetailView):
     model = Blog
+
+    # Увеличение счетчика просмотров
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.views_count += 1
+        self.object.save()
+        return self.object
+
 
 class BlogDeleteView(DeleteView):
     model = Blog
